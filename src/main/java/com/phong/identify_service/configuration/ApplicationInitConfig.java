1de2d02bf7f60e0d8dee7391bd54beaf407f3377
LAPTOP-OR7KQEPI\Admin <htphong191@gmail.com>
2025-10-29 13:11:59 +0900

    a

--- src/main/java/com/phong/identify_service/configuration/ApplicationInitConfig.java
+++ src/main/java/com/phong/identify_service/configuration/ApplicationInitConfig.java
@@ -7,8 +7,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,7 +25,11 @@ public class ApplicationInitConfig {
     private PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty(prefix = "spring.datasource",
+            name="driver-class-name",
+            havingValue = "org.postgresql.Driver")
     ApplicationRunner applicationRunner(UserRepository userRepository) {
+        log.info("App: Init application");
         return args -> {
             if (userRepository.findByUsername("admin").isEmpty()) {
                 var roles = new HashSet<String>();
